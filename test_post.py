eb0965cb7433a8b1db815f6dd807ea39b30e1b44
Jack@2093 <99194369+1972kashi@users.noreply.github.com>
2026-06-25 08:30:25 +0300

    A real commit message briefly summarizing the changes made in this commit.

--- test_post.py
+++
@@ -1,8 +0,0 @@
-import http.client, json
-conn = http.client.HTTPConnection('127.0.0.1', 8000)
-payload = json.dumps({"name":"Test Car","miles":"1000","trans":"Auto","fuel":"Petrol","year":"2022","price":"1000000","img":"Pic/test.jpg","badge":True})
-headers = {'Content-Type':'application/json'}
-conn.request('POST','/api/cars', body=payload, headers=headers)
-res = conn.getresponse()
-print(res.status, res.reason)
-print(res.read().decode())
